14436ba54a6f1f8f81c5ab1ec4614d084708dc6f
Asha Krishna S <68347359+AshaKrishnaS@users.noreply.github.com>
2021-11-16 10:37:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            display();
+        }
+        static void display()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
